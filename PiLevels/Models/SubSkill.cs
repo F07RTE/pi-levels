f1fb881662e54499596d89ddf889d56b268227b6
Guilherme <guilherme.forte17@gmail.com>
2025-03-26 13:22:37 -0300

    feat: add `SubSkill.cs`

---
+++ PiLevels/Models/SubSkill.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PiLevels.Models;
+
+public class SubSkill
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public List<Level> Levels { get; set; } = new List<Level>();
+}
